0a4284b1ea39b7fcccdde0de8a4ec643935d9e8a
Luke Thornton <725682@student.cityofbristol.ac.uk>
2024-09-15 13:07:24 +0100

    feat: init.py being updated with Flask

---
+++ __init__.py
@@ -0,0 +1,10 @@
+import os 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLACHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
