dc55f784dee0bfffe5b9088a3032ae2318ac4f0e
Luke Thornton <725682@student.cityofbristol.ac.uk>
2024-09-15 22:51:58 +0100

    feat: form update and signup.html added

--- datevault/routes.py
+++ datevault/routes.py
@@ -10,3 +10,7 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
