8a95be18b596de6a98c1576e27b587f981df44d3
Luke Thornton <725682@student.cityofbristol.ac.uk>
2024-09-15 21:59:50 +0100

    feat: login.html linked and route created

--- datevault/routes.py
+++ datevault/routes.py
@@ -6,3 +6,7 @@ from datevault.models import Login
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
