46e2a294c9b13c81d6789bc9bdc72613a231e5a0
Luke Thornton <725682@student.cityofbristol.ac.uk>
2024-09-15 16:15:43 +0100

    feat: partner side added to login model

--- datevault/models.py
+++ datevault/models.py
@@ -1,13 +1,15 @@
 from datevault import db
 
-class User(db.model):
-    #schema for the Login model
+class Login(db.model):
+    #schema for the login model
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.VARCHAR(50), unique=True, nullable=False)
     password = db.Column(db.VARCHAR(250), unique=False, nullable=False)
     email = db.Column(db.VARCHAR(100), unique=True, nullable=False)
+    #partner login details
     partner_user_name = db.Column(db.VARCHAR(50), unique=True, nullable=False)
     partner_password = db.Column(db.VARCHAR(250), unique=False, nullable=False)
+    partner_email = db.Column(db.VARCHAR(100), unique=True, nullable=False)
 
     def __repr__(self):
         #__repr__ to represent itself in the form of a string.
