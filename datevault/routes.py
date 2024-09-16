134f313f29c65a3c2a39b307a4d08d3f3066bb7b
Luke Thornton <725682@student.cityofbristol.ac.uk>
2024-09-16 22:15:14 +0100

    feat: first attempt at signup posting to db

--- datevault/routes.py
+++ datevault/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from datevault import app, db
 from datevault.models import Login
 
@@ -9,15 +9,22 @@ def home():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == "POST":
-        login = Login(
-                password=request.form.get("password"),
-                email=request.form.get("email"),
-        )
     return render_template("login.html")
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        signup = Login(
+            user_name=request.form.get("user_name") 
+            password=request.form.get("SignupPassword"),
+            email=request.form.get("email"),
+            partner_user_name="",
+            partner_password="",
+            partner_email=""
+        )
+        db.session.add(signup)
+        db.session.commit()
+        return redirect(url_for("login"))
     return render_template("signup.html")
 
 @app.route("/add_partner", methods=["GET", "POST"])
