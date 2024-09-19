b8d04c6cc75a3b9a1cb232ae6ab8afa5ed9bd348
Luke Thornton <725682@student.cityofbristol.ac.uk>
2024-09-19 23:22:55 +0100

    feat: flask migrations add

--- datevault/__init__.py
+++ datevault/__init__.py
@@ -1,6 +1,8 @@
 import os 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate  # Import Migrate
+
 if os.path.exists("env.py"):
     import env
 
@@ -9,5 +11,6 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)  # Initialize Migrate
 
-from datevault import routes #noqa
+from datevault import routes  # noqa
--- datevault/models.py
+++ datevault/models.py
@@ -3,13 +3,13 @@ from datevault import db
 class Login(db.Model):
     #schema for the login model
     id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.VARCHAR(50), unique=True, nullable=False)
-    password = db.Column(db.VARCHAR(250), unique=False, nullable=False)
-    email = db.Column(db.VARCHAR(100), unique=True, nullable=False)
+    user_name = db.Column(db.VARCHAR(50), unique=True, nullable=True)
+    password = db.Column(db.VARCHAR(250), unique=False, nullable=True)
+    email = db.Column(db.VARCHAR(100), unique=True, nullable=True)
     #partner login details
-    partner_user_name = db.Column(db.VARCHAR(50), unique=True, nullable=False)
-    partner_password = db.Column(db.VARCHAR(250), unique=False, nullable=False)
-    partner_email = db.Column(db.VARCHAR(100), unique=True, nullable=False)
+    partner_user_name = db.Column(db.VARCHAR(50), unique=True, nullable=True)
+    partner_password = db.Column(db.VARCHAR(250), unique=False, nullable=True)
+    partner_email = db.Column(db.VARCHAR(100), unique=True, nullable=True)
 
     def __repr__(self):
         #__repr__ to represent itself in the form of a string.
