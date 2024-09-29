70cf805380088ec068362187f2b67fdde44d25fb
Luke Thornton <725682@student.cityofbristol.ac.uk>
2024-09-29 20:11:50 +0100

    chore: native_enum added

--- datevault/models.py
+++ datevault/models.py
@@ -14,6 +14,7 @@ class Login(db.Model):
     partner_user_name = db.Column(db.VARCHAR(50), unique=True, nullable=True)
     partner_password_hash = db.Column(db.VARCHAR(250), unique=False, nullable=True)
     partner_email = db.Column(db.VARCHAR(100), unique=True, nullable=True)
+    dates = db.relationship("Date", backref="login", cascade="all, delete", lazy=True)
 
     # Set password method to hash the password before storing
     def set_password(self, password):
@@ -32,7 +33,7 @@ class Login(db.Model):
 
     def __repr__(self):
         #__repr__ to represent itself in the form of a string.
-        return self.user_name
+        return f"<Login user_name={self.user_name}>"
 
 
 # Define an Enum for time options
@@ -58,23 +59,23 @@ class ActivityOptions(enum.Enum):
     MODERATE = "Moderate"
     HIGH = "High"
 
-'''
+
 class Date(db.Model):
     # Schema for the Date Model
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.string(50), nullable=False)
-    is_time = db.Column(Enum(TimeOptions), nullable=False)
-    is_budget = db.Column(Enum(BudgetOptions), nullable=False)
-    is_location = db.Column(Enum(LocationOptions), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    is_time = db.Column(Enum(TimeOptions native_enum=False), nullable=False)
+    is_budget = db.Column(Enum(BudgetOptions native_enum=False), nullable=False)
+    is_location = db.Column(Enum(LocationOptions native_enum=False), nullable=False)
     is_dog = db.Column(db.Boolean, nullable=False)
-    is_activity = db.Column(Enum(ActivityOptions), nullable=False)
+    is_activity = db.Column(Enum(ActivityOptions native_enum=False), nullable=False)
     is_reservation = db.Column(db.Boolean, nullable=False)
     is_indoor = db.Column(db.Boolean, nullable=False)
     notes = db.Column(db.Text, nullable=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey("login.id", ondelete="Cascade"), nullable=False)
 
     def __repr__(self):
         #__repr__ to represent itself in the form of a string.
         return f"<Date id={self.id}, time={self.is_time.value}, budget={self.is_budget.value}, location={self.is_location.value}, dog_friendly={self.is_dog}, activity={self.is_activity.value}, reservation_required={self.is_reservation}, indoor={self.is_indoor}>"
 
 
-'''
