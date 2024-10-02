b2c7dc2e7a17192b2926a0890375e65de5a87502
Luke Thornton <725682@student.cityofbristol.ac.uk>
2024-10-02 22:19:56 +0100

    fix: removed numerous forms, now only 1 form, works.

--- datevault/routes.py
+++ datevault/routes.py
@@ -152,7 +152,7 @@ def new_idea():
         else:
             flash("Invalid Location Selection! How did you do that?","error")
             return redirect(url_for('new_idea'))
-        is_dog=request.form.get("is_dog")
+        is_dog=bool(request.form.get("is_dog"))
         is_activity=request.form.get("is_activity")
         # Checks to see if valid entry, protects database. Also converts is_activity to a string.
         if is_activity in ActivityOptions.__members__:
@@ -160,8 +160,8 @@ def new_idea():
         else:
             flash("Invalid Activity Selection! How did you do that?","error")
             return redirect(url_for('new_idea'))
-        is_reservation=request.form.get("is_reservation")
-        is_indoor=request.form.get("is_indoor")
+        is_reservation=bool(request.form.get("is_reservation"))
+        is_indoor=bool(request.form.get("is_indoor"))
         notes=request.form.get("notes")
 
         # Creates a new_date object
@@ -177,7 +177,7 @@ def new_idea():
             notes=notes,
             owner_id=session.get('user_id')
         )
-        db.session(new_date)
+        db.session.add(new_date)
         db.session.commit()
 
         flash("New date idea added successfully! Add another!", "success")
