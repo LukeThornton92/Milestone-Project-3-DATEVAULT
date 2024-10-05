671e5a477bb305e2eddd7b26332c22e58f68f10e
Luke Thornton <725682@student.cityofbristol.ac.uk>
2024-10-05 20:37:54 +0100

    feat: view all route query all dates

--- datevault/routes.py
+++ datevault/routes.py
@@ -247,9 +247,22 @@ def pick_a_date():
 
     return render_template("pick_a_date.html", no_date_check=no_date_check,  time_options=TimeOptions, budget_options=BudgetOptions, location_options=LocationOptions, activity_options=ActivityOptions) # no_date_check is passed to the html so jinja works!
 
-@app.route("/view_all")
+@app.route("/view_all", methods =["GET", "POST"])
 def view_all():
     '''
     displays all dates, best way of editing and deleting saved dates 
     '''
+    user_id=session.get('user_id')
+        
+    if not user_id:
+        flash("You need to log in to view your dates!", "error")
+        return redirect(url_for('login'))
+        
+    all_dates = Date.query.filter_by(owner_id=user_id).all()
+    
+    # Debugging
+    print(f"User ID: {user_id}")
+    print(f"All dates: {all_dates}")
+
+    
     return render_template("view_all.html")
